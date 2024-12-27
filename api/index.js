50d8dfb621e0df4d7977fb26498cdabb49b87dd2
masterthuku <peterthuku706@gmail.com>
2024-12-28 00:22:21 +0300

    test api routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRouter from './routes/user.routes.js'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB')
+}).catch((error) => {
+    console.log(error)
+})
 
 const app = express()
 
 app.listen(5000, () => {
     console.log('Server running on port 5000')
 })
+
+app.use('/api/user', userRouter)
