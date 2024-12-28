5a4a6da17f4efe254a68fe886846a826d7169939
masterthuku <peterthuku706@gmail.com>
2024-12-28 18:20:21 +0300

    another one

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
+  const hashedPassword = await bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password });
 
   await newUser.save();
